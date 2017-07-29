ac362ecf5a5d9de6cc4e2eeb5a021b04ba63d2ef
alimfeld <aimfeld@gmx.ch>
2017-07-30 00:02:02 +0200

    Add fallback image

--- public/script.js
+++ public/script.js
@@ -8,13 +8,13 @@ $(document).ready(function() {
       const meme = memes[id];
       if (meme) {
         console.info(meme);
-        $('img').attr('src', `memes/${meme.i}`);
+        $('img').attr('src', meme.i ? `memes/${meme.i}` : 'dummy.gif');
         $('p.top').text(meme.tt);
         $('p.bottom').text(meme.tb);
         $('footer').text(`${meme.s}: ${track.track_resource.name} - ${track.artist_resource.name}`);
       } else {
         console.log(`No meme for track with id '${id}' found.`);
-        $('img').attr('src', '');
+        $('img').attr('src', 'dummy.gif');
         $('p.top').text('');
         $('p.bottom').text('');
         $('footer').text('');
